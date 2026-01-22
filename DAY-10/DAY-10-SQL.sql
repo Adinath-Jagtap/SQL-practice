3338900d8a828041430c5fd5f922f6b4b042102a
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-22 20:45:53 +0530

    DAY-10

---
+++ DAY-10/DAY-10-SQL.sql
@@ -0,0 +1,10 @@
+-- =============================================================================
+-- SQL PROBLEMS (3)
+-- =============================================================================
+/*
+1. Find employees who earn more than their managers
+
+2. Rank employees by salary within each department
+
+3. Find departments that have more than 5 employees
+*/
